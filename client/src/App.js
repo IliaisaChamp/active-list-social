09124f73b2fc0d8a2511e48e6924f5c4bdc65e6c
Alex <eriocl@ya.ru>
2021-11-24 10:14:03 +0300

    sockets refactored

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,40 @@
-import { useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { checkUser, deleteUser } from "./store/ac/authAC";
-import io from "socket.io-client";
+import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkUser, deleteUser } from './store/ac/authAC';
+import io from 'socket.io-client';
 // routes
-import Router from "./routes";
+import Router from './routes';
 
 // theme
-import ThemeConfig from "./theme";
-import GlobalStyles from "./theme/globalStyles";
+import ThemeConfig from './theme';
+import GlobalStyles from './theme/globalStyles';
 
 // components
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 // delete
-import { BaseOptionChartStyle } from "./components/charts/BaseOptionChart";
-import FlashMessage from "./components/FlashMessage/FlashMessage";
-import Notification from "./components/Notification/Notification";
-import axios from "axios";
-import { createSocketConnect, setOnline } from "./store/ac/onlineUsersAc";
-import { setSocket } from "./store/ac/socketAc";
-import { setNewReportNotification } from "./store/ac/notificationAC";
-import useSocket from "./hooks/useSocket";
-
-
+import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
+import FlashMessage from './components/FlashMessage/FlashMessage';
+import Notification from './components/Notification/Notification';
+import axios from 'axios';
+import useSocket from './hooks/useSocket';
 
 function App() {
-  useSocket();
   const dispatch = useDispatch();
+  useSocket(dispatch);
 
   axios.defaults.withCredentials = true;
-
-  axios.defaults.baseURL = "http://localhost:3001";
-
-
-
+  axios.defaults.baseURL = 'http://localhost:3001';
   axios.interceptors.response.use(
     (res) => res,
     (err) => {
-      console.log("intereceptor USED");
+      console.log('intereceptor USED');
       if (err.status === 401) {
         dispatch(deleteUser);
       }
       return Promise.reject(err);
-    }
+    },
   );
-
-
-
-  // useEffect(() => {
-  //   dispatch(createSocketConnect(socket));
-  //   return () => socket.current.emit('logout');
-  // }, [socket, dispatch]);
 
   useEffect(() => {
     dispatch(checkUser());
@@ -75,7 +59,7 @@ function App() {
   //   // return () => socket.current.emit('logout');
   // }, [user, dispatch]);
 
-  console.log("APP RENDER");
+  console.log('APP RENDER');
   return (
     <ThemeConfig>
       <ScrollToTop />
--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -1,32 +1,31 @@
-import io from "socket.io-client";
-import { setNewReportNotification } from "../store/ac/notificationAC";
-import { createSocketConnect, setOnline } from "../store/ac/onlineUsersAc";
-import { useSelector, useDispatch } from "react-redux";
-import { useRef } from "react";
+import io from 'socket.io-client';
+import { setNewReportNotification } from '../store/ac/notificationAC';
+import { createSocketConnect, setOnline } from '../store/ac/onlineUsersAc';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useRef } from 'react';
 
-export default function useSocket() {
+export default function useSocket(dispatch) {
   const socket = useRef();
-  const dispatch = useDispatch();
-
   const user = useSelector((state) => state.user);
 
-  if (user) {
-    socket.current = io("http://localhost:3001", {
-      query: { id: user.id },
-    });
-    socket.current.on("notification", (msg) => {
-      console.log("NOTIFICATION", msg);
-      dispatch(setNewReportNotification(msg.message));
-    });
-    socket.current.on("broadcast-online", (msg) => {
-      console.log("USERS ONLINE", msg);
-      dispatch(setOnline(msg.users));
-    });
-
-
-    dispatch(createSocketConnect(socket, user));
-  }
+  useEffect(() => {
+    if (user) {
+      socket.current = io('http://localhost:3001', {
+        query: { id: user.id },
+      });
+      socket.current.on('notification', (msg) => {
+        console.log('NOTIFICATION', msg);
+        dispatch(setNewReportNotification(msg.message));
+      });
+      socket.current.on('broadcast-online', (msg) => {
+        console.log('USERS ONLINE', msg);
+        dispatch(setOnline(msg.users));
+      });
+      dispatch(createSocketConnect(socket, user));
+    }
+    return () => socket?.current?.emit('logout')
+  }, [user, dispatch]);
   return {
-    socket
-  }
+    socket,
+  };
 }
