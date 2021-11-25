fb1434bc5a936846500dc5552340b2952ddbdc6f
SSK <sskolcov@gmail.com>
2021-11-25 23:20:18 +0300

    top-full-sort-ultra-top-hight

--- client/src/components/TopFolder/TopPostsSort.js
+++ client/src/components/TopFolder/TopPostsSort.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { MenuItem, TextField } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { setAllReportsForTopSortedByComments, setAllReportsForTop, setAllReportsForTopSortedByLikes } from '../../store/ac/reportsAC';
+import { useState } from 'react';
 
 
 
@@ -15,17 +16,23 @@ TopPostsSort.propTypes = {
 
 export default function TopPostsSort({ options }) {
   const dispatch = useDispatch()
+  const [stateValue, setstateValue] = useState('Проcматриваемые')
   const clickHandler = (e) => {
-
-    if (e.target.value === "Комментируемые") {
-      dispatch(setAllReportsForTopSortedByComments())
-    }
-    else if (e.target.value === "Просматриваемые") {
+    if (e.target.value === "Популярные") {
+      setstateValue('Популярные')
       dispatch(setAllReportsForTopSortedByLikes())
     }
+    if (e.target.value === "Комментируемые") {
+      setstateValue('Комментируемые')
+      dispatch(setAllReportsForTopSortedByComments())
+    }
+    if (e.target.value === "Проcматриваемые") {
+      setstateValue('Проcматриваемые')
+    }
+    
   }
   return (
-    <TextField select size="small" value="Популярные" onChange={clickHandler}>
+    <TextField select size="small" value={stateValue} onChange={clickHandler}>
       {options.map((option) => (
         <MenuItem key={option.value} value={option.value}>
           {option.label}
