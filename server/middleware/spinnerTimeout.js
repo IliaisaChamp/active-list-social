97b349a725d8745bae91365d931a495bfdd4fc36
IliaisaChamp <ilia.isachamp@gmail.com>
2021-11-26 00:00:32 +0300

    middleware timeout

--- server/middleware/spinnerTimeout.js
+++ server/middleware/spinnerTimeout.js
@@ -1,7 +1,12 @@
 function spinnerTimeout(req, res, next) {
+  console.log(req.url);
+  if (req.url.toString().includes('/like') || req.url.toString().includes('/comment')) {
+    next();
+  } else {
     setTimeout(() => {
       next();
     }, 1000);
+  }
 }
 
 module.exports = spinnerTimeout;
